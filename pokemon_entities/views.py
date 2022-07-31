c806a3acefd8caec5ba3e956eb76067bb8a7fcc0
nankuF <fireloki87@gmail.com>
2022-07-31 15:59:51 +0600

    use method get.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -52,12 +52,10 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    pokemons = Pokemon.objects.all()
+    pokemon = Pokemon.objects.get(id=pokemon_id)
 
-    for pokemon in pokemons:
-        if pokemon.id == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
+    if pokemon.id == int(pokemon_id):
+        requested_pokemon = pokemon
     else:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
